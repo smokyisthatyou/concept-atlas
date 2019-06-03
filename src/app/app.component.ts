028ed3177006181671e962e6b91fdd90133e6a1b
Giorgia Manna <manna.giorgia@educ.di.unito.it>
2019-06-03 13:32:44 +0200

    switch button login-logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import {AuthService, GoogleLoginProvider} from 'angularx-social-login';
-import {HttpConnectionService} from './common/http-connection.service';
+import { AuthService, GoogleLoginProvider } from 'angularx-social-login';
+import { HttpConnectionService } from './common/http-connection.service';
 import { catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
 
@@ -11,24 +11,31 @@ import { of } from 'rxjs';
 })
 export class AppComponent {
   title = 'ConceptAtlas';
+  isLogged: boolean;
+  constructor(private socialAuthService: AuthService, private httpConn: HttpConnectionService) {
+    const currentUser = this.httpConn.currentUserValue;
+    if (currentUser) {
+      this.isLogged = true;
+    }
+  }
 
-  constructor( private socialAuthService: AuthService, private httpConn: HttpConnectionService ) {}
+
 
 
   public signinWithGoogle() {
     const socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
 
     this.socialAuthService.signIn(socialPlatformProvider)
-        .then((userData) => {
-          // on success
-          // this will return user data from google. What you need is a user token which you will send it to the server
-          this.httpConn.sendToRestApiMethod(userData.idToken);
-        });
+      .then((userData) => {
+        // on success
+        // this will return user data from google. What you need is a user token which you will send it to the server
+        this.httpConn.sendToRestApiMethod(userData.idToken);
+      });
   }
 
   logOut() {
     // tslint:disable-next-line:only-arrow-functions
-    this.socialAuthService.signOut().then(function() {
+    this.socialAuthService.signOut().then(function () {
       window.location.assign('http://localhost:4200');
     });
     this.httpConn.resetUser();
