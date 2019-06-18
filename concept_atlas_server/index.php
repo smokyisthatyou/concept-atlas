fbeb9d45dcc8537c092ab7b0e1d6da44cef2674b
Sibilla <merlo.sibilla@educ.di.unito.it>
2019-06-18 21:50:19 +0200

    new index.php

---
+++ concept_atlas_server/index.php
@@ -0,0 +1,68 @@
+<?php
+
+use DB\SQL;
+
+require_once('./vendor/autoload.php');
+
+
+header('Access-Control-Allow-Origin: http://localhost:4200');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+$f3 = Base::instance();
+
+//google auth
+$f3->set('CLIENT_ID', '815639073197-4nm8nn59b9amkgtlsd3eanf65gue20ll.apps.googleusercontent.com');
+
+// db setup
+$dbConn = 'mysql:host=127.0.0.1;port=3306;dbname=conceptatlas';
+$dbUser = 'root';
+$dbPassword = '';
+$f3->set('DB', new \DB\SQL($GLOBALS['dbConn'], $GLOBALS['dbUser'], $GLOBALS['dbPassword']));
+
+
+//login auth
+$f3->route('POST /login',
+    function ($f3) {
+        $body = json_decode($f3->get('BODY'));
+        $token = $body->token;
+        $client = new Google_Client(['client_id' => $f3->get('CLIENT_ID')]);
+        $payload = $client->verifyIdToken($token); //send the token id to the server for verification
+        if (!$payload) {
+            $f3->error(500, "Invalid user token");
+            return;
+        }
+
+        $email = $payload['email'];
+
+        $db = $f3->get('DB');
+
+
+        $users = $db->exec('SELECT * FROM users '
+            . 'WHERE email=?', $email);
+        if (count($users) != 1) {
+            // User is not registered on server
+            // Refuse
+            echo json_encode(['email' => ""]);
+        } else
+            echo json_encode($users[0]);
+    });
+
+$f3->route('GET /atlas',
+    function ($f3) {
+        $db = $f3->get('DB');
+
+        $allAtlas = $db->exec('SELECT id, name, description, owner FROM atlas ');
+        echo json_encode($allAtlas);
+    });
+
+$f3->route('GET /mapworks/@atlasid',
+    function ($f3) {
+        $db = $f3->get('DB');
+        $atlasid = $f3->get('PARAMS.atlasid');
+        $mapworks = $db->exec('SELECT * FROM mapwork WHERE mapwork.atlas = ?',[$atlasid]);
+        echo json_encode($mapworks);
+    });
+
+$f3->run();
+?>
